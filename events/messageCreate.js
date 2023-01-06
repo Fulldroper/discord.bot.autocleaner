a8465e9236e89f63019c642dbed7f355c2ab5fcf
full_droper <full_droper@pm.me>
2023-01-06 13:35:42 +0200

    upd

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,6 @@
 module.exports = async function (msg) {
-  const {time = -1} = await this.db.get(`${this.user.username}:${msg.guildId}:${msg.channelId}`)
-  if (time < 0) return
+  const d = await this.db.get(`${this.user.username}:${msg.guildId}:${msg.channelId}`)
+  if (!d || d.time < 0) return
   setTimeout(() => {
     msg.delete()
   }, time * 1000);
